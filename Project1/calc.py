c5e42365ce32d09733613e78ac5a1a66adc1d887
RichardBangari <richard.bangari@dossiefoyer.in>
2025-07-15 15:45:47 +0530

    Update calc.py

--- Project1/calc.py
+++ Project1/calc.py
@@ -18,7 +18,7 @@ def div(a,b):
     z=0
     z=a/b
     print(z)'''
-numbers = list(range(0, 51, 4))  # [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48]
+numbers = list(range(0, 51, 2))  # [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48]
 results = []
 for number in numbers:
     if not number % 3:          # i.e., number % 3 == 0
